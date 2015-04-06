475b0de3b3de9a90d9dae340bc6d285bdf2b2fe7
Jessica Lee <JLee@Jessicas-MacBook-Air-3.local>
2015-04-05 21:12:18 -0400

    fully working!

--- app/src/main/java/com/example/jlee/equityeatsproject/CallAPI.java
+++ app/src/main/java/com/example/jlee/equityeatsproject/CallAPI.java
@@ -13,48 +13,71 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import android.os.StrictMode;
-
-
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ListView;
+import java.util.List;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import java.util.ArrayList;
+import android.widget.BaseAdapter;
 
 public class CallAPI extends ActionBarActivity {
 
 
-
+    String searchFor;
+    ArrayList<String> output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_api);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        String output =GetURL.getParameterKey("jessica+lee");
+        StrictMode.setThreadPolicy(policy);
+        String searchFor = MainActivity.searchFor;
 
-        TextView t= (TextView)findViewById(R.id.FromITUNES);
-        t.setText(output);
-
-
-
+        output = GetURL.getParameterKey(searchFor);
+        if (output!=null) {
+            getListView();
+        }
     }
+
+
+
+        protected void getListView() {
+
+            ArrayAdapter adapter = new ArrayAdapter<String>(
+                    this,
+                android.R.layout.simple_list_item_1, output);
+
+
+            ListView listview;
+            listview = (ListView) findViewById(R.id.listView);
+
+            listview.setAdapter(adapter);
+           adapter.notifyDataSetChanged();
+        }
+
+/// get a scroll bar
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_call_api, menu);
+      getMenuInflater().inflate(R.menu.menu_call_api, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+  //  @Override
+  // public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+     // int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+      //  noinspection SimplifiableIfStatement
+     //        return true;
+      // }
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+      //  return super.onOptionsItemSelected(item);
+   }
+
--- app/src/main/java/com/example/jlee/equityeatsproject/GetURL.java
+++ app/src/main/java/com/example/jlee/equityeatsproject/GetURL.java
@@ -1,13 +1,20 @@
 package com.example.jlee.equityeatsproject;
-
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.ArrayList;
+import android.util.JsonReader;
+
 
 /**
  * Created by JLee on 4/2/15.
@@ -15,61 +22,99 @@ import java.net.URLConnection;
 public class GetURL {
 
 
-        public static String getParameterKey(String search)
-        {
+    public static ArrayList<String> getParameterKey(String search)  {
+
+        String parameters = search.replaceAll(" ", "+");
+
+        String urlBase = "https://itunes.apple.com/search?";
+        String key1 = "term";
+        String key2 = "country";
 
 
-            // replace all spaces with "+"
+        String link = (urlBase + key1 + "=" + parameters + "&" + key2 + "=" + "US");
+        URL url;
+        StringBuilder builder;
 
-            for (int i = 0; i < search.length(); i++) {
+        String line;
+        String output = "";
+        String songName;
+        JSONArray bigArray = null;
+        JSONObject individualObject = null;
+        JSONObject json2;
+        JSONObject singleResponse= null;
+        String wrapperType= "";
+        String artistName="";
+        String kind = "";
+        int artistId = 0;
+        int trackId = 0;
+        String collectionName = "";
+        String trackName = "";
+        //String[] responseList = {"empty"};
+        ArrayList<String> responseList = new ArrayList<>();
 
-                char searchIndex = search.charAt(i);
-                if (searchIndex == ' ') {
-                    searchIndex = '+';
+        try {
+            url = new URL(link);
+
+            URLConnection connection = url.openConnection();
+            // example from Google Code JSON to Java -- https://developers.google.com/image-search/v1/jsondevguide
+            String urlString = "";
+            builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+             json2 = new JSONObject(builder.toString());
+
+
+            bigArray = json2.getJSONArray("results");
+           JSONObject[] arrays = new JSONObject[bigArray.length()];
+
+             // get the objects within the JSON array
+            for (int i = 0; i < bigArray.length() ; i++) {
+
+                JSONObject childJSONObject = bigArray.getJSONObject(i);
+
+                arrays[i]=childJSONObject;
+
+            }
+
+                for (int j = 0; j < arrays.length; j++) {
+
+                    JSONObject child = arrays[j];
+
+                    wrapperType = child.getString("wrapperType");
+                    artistName = child.getString("artistName");
+                    kind = child.getString("kind");
+                    trackId = child.getInt("trackId");
+                    trackName = child.getString("trackName");
+
+                    // singleResponse = new JSONObject();
+                    // singleResponse.put("wrapperType", wrapperType);
+                    //singleResponse.put("artistName", artistName);
+                    // singleResponse.put("kind", kind);
+                    // singleResponse.put("trackId", trackId);
+                    //singleResponse.put ("trackName", trackName);
+                    //   String result = singleResponse.toString();
+                    String temp = "wrapperType: " + wrapperType + "\n" + "artistName: " + artistName +
+                            "\n" + "kind: " + kind + "\n" + "trackId: " + trackId + "\n" + "trackName: " +
+                            trackName + "\n" ;
+
+                    responseList.add(temp);
+
 
                 }
-            }
-            String urlBase = "https://itunes.apple.com/search?";
-            String key1 = "term";
-            String key2 = "country";
 
-
-            String link= (urlBase + key1 + "=" + search + "&" + key2 + "=" + "US");
-            URL url;
-            StringBuilder builder;
-            String line;
-            JSONObject json;
-            String output;
-            try {
-                url = new URL(link);
-
-                URLConnection connection = url.openConnection();
-
-                String urlString = "";
-                builder = new StringBuilder();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-
-                json = new JSONObject(builder.toString());
-                output = json.toString();
-            }
-            catch (IOException e)
-
-            {
-                output= "THIS DIDN'T WORK";
-
-            }
-
-            catch(JSONException j)
-            {
-                output = "WRONG!";
-            }
-
-            return output;
         }
+        catch (Exception j) {
+            output = "We could not process your request";
 
+        }
+       // catch (IOException e) {
+        //    output = "We could not process your request";
+       // }
 
-
+        return responseList;
     }
+
+}
